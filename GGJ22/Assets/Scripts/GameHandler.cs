fb9682eb89b0594542a73f2731c25f049d1c65b4
NixAJ <nickiarpejensen@gmail.com>
2022-01-29 17:26:16 +0100

    Implemented GameHandler (Check HackNPlan for specifics)
    Added Debug Input to Bootstrap for (LoadPrevMap, LoadNextMap, ResetMap)
    Removed Camera from Bootstrap Scene

--- GGJ22/Assets/Scripts/GameHandler.cs
+++ GGJ22/Assets/Scripts/GameHandler.cs
@@ -1,4 +1,8 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameInformation
 {
@@ -6,10 +10,14 @@ public class GameInformation
     public int maxUnlockedLevel = 0;
 }
 
+public struct GameLevelScene
+{
+    public int buildIndex;
+    public string name;
+}
+
 public class GameHandler
 {
-    public GameInformation gameInformation;
-
     public void Initialize(Bootstrap bootstrap)
     {
         gameInformation = new GameInformation
@@ -18,18 +26,105 @@ public class GameHandler
             maxUnlockedLevel = PlayerPrefs.GetInt("MaxUnlockedLevel", 0)
         };
 
+        int sceneCount = SceneManager.sceneCountInBuildSettings;
+        _gameLevelScenes = new List<GameLevelScene>(sceneCount);
+
+        for (int i = 0; i < sceneCount; i++)
+        {
+            string sceneName = Path.GetFileNameWithoutExtension(SceneUtility.GetScenePathByBuildIndex(i));
+            if (!sceneName.StartsWith("Level"))
+                continue;
+
+            GameLevelScene levelScene = new GameLevelScene();
+            levelScene.buildIndex = i;
+            levelScene.name = sceneName;
+
+            _gameLevelScenes.Add(levelScene);
+        }
+
+        // Sort Levels based on name in alphabetical order
+        _gameLevelScenes.Sort((x, y) => x.name.CompareTo(y.name));
+
+
+        // Ensure CurrentLevel && MaxUnlockedLevel is between 0 -> sceneCount
+        gameInformation.currentLevel = Math.Min(gameInformation.currentLevel, sceneCount);
+        gameInformation.currentLevel = Math.Max(gameInformation.currentLevel, 0);
+        gameInformation.maxUnlockedLevel = Math.Min(gameInformation.maxUnlockedLevel, sceneCount);
+        gameInformation.maxUnlockedLevel = Math.Max(gameInformation.maxUnlockedLevel, 0);
+
+        int currentLevel = gameInformation.currentLevel;
+        LoadLevel(currentLevel);
+
         bootstrap.OnDestroy += Bootstrap_OnDestroy;
         bootstrap.OnApplicationQuit += Bootstrap_OnApplicationQuit;
     }
 
-    private void Bootstrap_OnDestroy()
+    public bool IsLevelValid(int level)
     {
-        SaveSettings();
+        return level >= 0 && level < _gameLevelScenes.Count;
     }
-
-    private void Bootstrap_OnApplicationQuit()
+    public bool LoadLevel(int level)
     {
-        SaveSettings();
+        if (!IsLevelValid(level))
+            return false;
+
+        GameLevelScene scene = _gameLevelScenes[level];
+        SceneManager.LoadSceneAsync(scene.buildIndex, LoadSceneMode.Additive);
+        return true;
+    }
+    public bool UnloadLevel(int level)
+    {
+        if (!IsLevelValid(level))
+            return false;
+
+        GameLevelScene scene = _gameLevelScenes[level];
+        SceneManager.UnloadSceneAsync(scene.buildIndex);
+        return true;
+    }
+    public bool LoadNextMap()
+    {
+        int currentLevel = gameInformation.currentLevel;
+        int nextLevel = gameInformation.currentLevel + 1;
+
+        bool isValidMaps = IsLevelValid(currentLevel) && IsLevelValid(nextLevel);
+        if (isValidMaps)
+        {
+            UnloadLevel(currentLevel);
+            LoadLevel(nextLevel);
+
+            gameInformation.currentLevel += 1;
+            gameInformation.maxUnlockedLevel = Math.Max(gameInformation.currentLevel, gameInformation.maxUnlockedLevel);
+            SaveSettings();
+        }
+
+        return isValidMaps;
+    }
+    public bool LoadPrevMap()
+    {
+        int currentLevel = gameInformation.currentLevel;
+        int prevLevel = gameInformation.currentLevel - 1;
+
+        bool isValidMaps = IsLevelValid(currentLevel) && IsLevelValid(prevLevel);
+        if (isValidMaps)
+        {
+            UnloadLevel(currentLevel);
+            LoadLevel(prevLevel);
+
+            gameInformation.currentLevel -= 1;
+            SaveSettings();
+        }
+
+        return isValidMaps;
+    }
+    public void ResetMap()
+    {
+        int currentLevel = gameInformation.currentLevel;
+
+        if (IsLevelValid(currentLevel))
+        {
+            UnloadLevel(currentLevel);
+            LoadLevel(currentLevel);
+        }
     }
 
     public void SaveSettings()
@@ -39,4 +134,16 @@ public class GameHandler
 
         PlayerPrefs.Save();
     }
+
+    private void Bootstrap_OnDestroy()
+    {
+        SaveSettings();
+    }
+    private void Bootstrap_OnApplicationQuit()
+    {
+        SaveSettings();
+    }
+
+    public GameInformation gameInformation;
+    private List<GameLevelScene> _gameLevelScenes;
 }
--- GGJ22/Assets/Scripts/Utils/Bootstrap.cs
+++ GGJ22/Assets/Scripts/Utils/Bootstrap.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public delegate void BootstrapEventHandler();
@@ -14,6 +15,23 @@ public class Bootstrap : MonoBehaviour
         EntityManager entityManager = new EntityManager();
         entityManager.Initialize(this);
         ServiceLocator.SetEntityManager(entityManager);
+    }
+    private void Update()
+    {
+        GameHandler gameHandler = ServiceLocator.GetGameHandler();
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            gameHandler.ResetMap();
+        }
+        else if (Input.GetKeyDown(KeyCode.N))
+        {
+            gameHandler.LoadNextMap();
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            gameHandler.LoadPrevMap();
+        }
     }
 
     public event BootstrapEventHandler OnDestroy;
