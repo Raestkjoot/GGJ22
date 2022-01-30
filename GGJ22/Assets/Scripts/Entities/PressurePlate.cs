b687a42335ddd96f20df0c1c904079ae413924c6
Carl Warming <carl.warming@gmail.com>
2022-01-30 15:01:48 +0100

    Added animation of pressure plate and made prefab

--- GGJ22/Assets/Scripts/Entities/PressurePlate.cs
+++ GGJ22/Assets/Scripts/Entities/PressurePlate.cs
@@ -7,6 +7,8 @@ public class PressurePlate : MonoBehaviour
 {
      private void Start()
     {
+        _animator = gameObject.GetComponentInChildren<Animator>();
+
         if (Argument == null)
         {
             Argument = true;
@@ -21,7 +23,7 @@ public class PressurePlate : MonoBehaviour
         if (other.tag != "Player")
             return;
         
-        Debug.Log("123");
+        _animator.SetBool("BeingPressed", true);
 
         EntityManager entityManager = ServiceLocator.GetEntityManager();
 
@@ -34,6 +36,8 @@ public class PressurePlate : MonoBehaviour
     }
     private void OnTriggerExit2D(Collider2D other) 
     {
+        _animator.SetBool("BeingPressed", false);
+
         EntityManager entityManager = ServiceLocator.GetEntityManager();
 
         Entity otherEntity = entityManager.GetByInstanceId(Interactable.transform.GetInstanceID());
@@ -48,5 +52,6 @@ public class PressurePlate : MonoBehaviour
 
     // For static Interactables false is until stepped off while true is permanent off
     [SerializeField] private bool Argument;
+    private Animator _animator; 
     private Entity _entity;
 }
